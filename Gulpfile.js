d1f2d1c46fc2594b24bddc4d623b2f0af974c7bf
Theuy Limpanont <info@theuy.nl>
2016-04-20 11:52:28 +0200

    feat(dist): build in per theme folder structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,29 +5,38 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
 var gutil = require('gulp-util');
-var gulpCopy = require('gulp-copy');
 var runSequence = require('gulp-run-sequence');
 var path = require('path');
+var _ = require('lodash');
+
+var themes = ['flatly', 'darkly'];
+var buildTaskNames = [];
+
+themes.forEach(function (theme) {
+    var sassTaskName = 'sass:' + theme;
+    var fontTaskName = 'fonts:' + theme;
+
+    gulp.task(sassTaskName, function () {
+        return gulp.src(path.resolve(__dirname, 'themes/_' + theme + '.scss'))
+            .pipe(rename('style.css'))
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest(path.resolve(__dirname, 'dist/' + theme + '/')));
+    });
+
+    gulp.task(fontTaskName, function () {
+        return gulp.src(path.resolve(__dirname, 'node_modules/bootstrap-sass/assets/fonts/**/*'))
+            .pipe(gulp.dest(path.resolve(__dirname, 'dist/' + theme + '/fonts/')));
+    });
+
+    buildTaskNames.push([sassTaskName, fontTaskName]);
+});
+
 
 gulp.task('clean:dist', function () {
-  return gulp.src(path.resolve(__dirname, 'dist'))
-  	.pipe(clean().on('error', gutil.log));
-});
-
-gulp.task('sass:bootstrap-theuy', function () {
-  return gulp.src(path.resolve(__dirname, 'stylesheets/bootstrap-theuy.scss'))
-  	.pipe(rename('bootstrap-theuy.css'))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(path.resolve(__dirname, 'dist/css')));
-});
-
-gulp.task('copy:fonts', function () {
-  return gulp.src(
-    [path.resolve(__dirname, 'node_modules/bootstrap-sass/assets/fonts/**/*')]
-  )
-  	.pipe(gulp.dest(path.resolve(__dirname, 'dist/fonts/')));
+    return gulp.src(path.resolve(__dirname, 'dist'))
+        .pipe(clean().on('error', gutil.log));
 });
 
 gulp.task('build', function(cb) {
-    runSequence('clean:dist', ['sass:bootstrap-theuy', 'copy:fonts'], cb)
+    runSequence('clean:dist', _.flatten(buildTaskNames), cb);
 });
